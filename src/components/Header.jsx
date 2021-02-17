d6671f978f209ebc4d4fecc4c2e9cf0cf2467df5
Ingo Peters <ingo.peters2@gmx.de>
2021-02-17 16:38:19 +0100

    - Setup project - Import bootstrap - Created a bootstrap header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Peters Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
